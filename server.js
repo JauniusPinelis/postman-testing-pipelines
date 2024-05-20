e44abea1f7448d3962f123526692c910e3de0401
JauniusPinelis <jauniuspinelis@gmail.com>
2024-05-20 18:08:39 +0300

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 // Routes
 const userRoutes = require('./routes/user');
-app.use('/api/users', userRoutes);
+app.use('/api/customers', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
